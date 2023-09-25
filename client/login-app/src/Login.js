0d2949e8adc2645e8cbd1d90256f986c036aafe8
medlaqraa <139716670+medlaqraa@users.noreply.github.com>
2023-09-25 17:34:32 +0100

    now change

--- client/login-app/src/Login.js
+++ client/login-app/src/Login.js
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Prev } from 'react-bootstrap/esm/PageItem';
 import { Link } from 'react-router-dom'
 
 export default function Login() {
+
+  const [values , setValues] = useState({
+    email : '',
+    password :'',
+  })
+
+  const handleSubmit =(event) => {
+    event.preventDefault();
+  }
+
+  const handleInput = (event) => {
+    setValues(prev => ({...prev,[event.target.name]:[event.target.value]}))
+  }
+
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
       <div className='bg-white p-3 rounded w-25'>
       <h2>Login</h2>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
             <div className='mb-3'>
                 <label htmlFor="email"><strong>Email</strong></label>
-                <input type="email" placeholder='Enter Your Email' className='form-control rounded-0'/>
+                <input type="email" onChange={handleInput} name='email' placeholder='Enter Your Email' className='form-control rounded-0'/>
             </div>
             
             <div className='mb-3'>
                 <label htmlFor="password"><strong>Password</strong></label>
-                <input type="password" placeholder='Enter Your Password' className='form-control rounded-0'/>
+                <input type="email" onChange={handleInput} name='password' placeholder='Enter Your Email' className='form-control rounded-0'/>
+                <input type="password"  placeholder='Enter Your Password' className='form-control rounded-0'/>
             </div>
 
-            <button className='btn btn-success w-100'>Login</button>
+            <button type='submit' className='btn btn-success w-100'>Login</button>
             <p>You are agree to our terms and policies</p>
             <Link to='/signup' className='btn btn-default border w-100 bg-light text-decoration-none'>Create Account</Link>
         </form>
---
+++ client/login-app/src/LoginValidation.js
@@ -0,0 +1,4 @@
+function LoginValidation(values) {
+ 
+}
+
